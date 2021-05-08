2c08dc12732d46031cd41708c95b6ff4ebf7fd02
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-05-08 21:53:26 +0200

    Deploy 1.0.1

--- googleSearch/blogspotSearch.py
+++ googleSearch/blogspotSearch.py
@@ -25,7 +25,7 @@ def getSearches(query):
             link = e.links[4].href
             fetch_count = fetch_count + 1
             page = urllib.request.urlopen(link)
-            soup = BeautifulSoup(page, from_encoding="utf-8", features="lxml")
+            soup = BeautifulSoup(page, from_encoding="utf-8")
             content = soup.get_text()
             blog_posts.append({'title': e.title, 'content': content, 'link': link})
 
