b04fdcc7e3e7d912b4873fe499efd89d61d6aae4
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-05-08 21:56:29 +0200

    Deploy 1.0.1 without blogspot

--- googleSearch/views.py
+++ googleSearch/views.py
@@ -8,10 +8,10 @@ def home(request):
     if request.POST:
         form_data = request.POST.dict()
         #google_result = googleSearch.get_searches(form_data.get('search'))
-        results = blogspotSearch.getSearches(form_data.get('search'))
+        #results = blogspotSearch.getSearches(form_data.get('search'))
         custom = customSearch.get_searches(form_data.get('search'))
         return render(request, '../templates/googleSearchScraperTemplates/home.html',
-                      {'results': custom + results})
+                      {'results': custom})
     else:
         return render(request, '../templates/googleSearchScraperTemplates/home.html')
 
