73fac7336404e3b81a948b778cae3fef54db6bf2
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-05-09 20:15:46 +0200

    Deploy 1.0.0 added gspress

--- googleSearch/customSearch.py
+++ googleSearch/customSearch.py
@@ -4,20 +4,23 @@ from bs4 import BeautifulSoup
 
 def get_searches(query):
     originalQuery = query
+    results = []
 
     # LIKA-ONLINE.COM
+    page_num = 1
     query = query.replace('č', '%C4%8D')
     query = query.replace('ć', '%C4%87')
     query = query.replace(' ', '+')
-    url = f"https://www.lika-online.com/?s={query}"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    job_elems = soup.find_all('div', class_='tdb_module_loop td_module_wrap td-animation-stack')
-    results = []
-    for job_elem in job_elems:
-        title = job_elem.find('a')['title']
-        link = job_elem.find('a')['href']
-        results.append({'title': title, 'content': 'snippet', 'link': link})
+    while page_num < 6:
+        url = f"https://www.lika-online.com/page/{page_num}/?s={query}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        job_elems = soup.find_all('div', class_='tdb_module_loop td_module_wrap td-animation-stack')
+        for job_elem in job_elems:
+            title = job_elem.find('a')['title']
+            link = job_elem.find('a')['href']
+            results.append({'title': title, 'content': 'snippet', 'link': link})
+        page_num += 1
 
     # LIKAPLUS.HR
     query = originalQuery.replace(' ', '%20')
@@ -59,4 +62,19 @@ def get_searches(query):
         link = job_elem.find('a')['href']
         results.append({'title': 'PROMISE.HR', 'content': 'snippet', 'link': link})
 
+    # GSPRESS.NET
+    page_num = 1
+    query = originalQuery.replace(' ', '+')
+    query = query.replace('č', '%C4%8D')
+    query = query.replace('ć', '%C4%87')
+    while page_num < 5:
+        url = f"https://www.gspress.net/page/{page_num}/?s={query}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        job_elems = soup.find_all('div', class_='td_module_16 td_module_wrap td-animation-stack')
+        for job_elem in job_elems:
+            title = job_elem.find('a')['title']
+            link = job_elem.find('a')['href']
+            results.append({'title': title, 'content': 'snippet', 'link': link})
+        page_num += 1
     return results
